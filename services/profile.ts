aecc79770b48d3e97e46fb94e22a65e9cb0450c2
ram33sul <rameesyounus49@gmail.com>
2023-06-07 14:29:05 +0530

    primary skill added in registerAsWorkerService

--- services/profile.ts
+++ services/profile.ts
@@ -63,21 +63,33 @@ export const editProfileService = ({
 interface RegisterAsWorkerService {
     bio: string, 
     age: number,
-    categoryList: string[],
+    categoryList: {
+        id: string,
+        hourlyWage: number,
+        weeklyWage: number
+    }[],
     userId: mongoose.Types.ObjectId,
     firstName: string,
     lastName: string,
     email: string,
     gender: boolean,
-    openToWork: boolean
+    openToWork: boolean,
+    primarySkill: string
 }
-export const registerAsWorkerService = ({bio, age, categoryList, userId, firstName, lastName, email, gender, openToWork}: RegisterAsWorkerService) => {
+export const registerAsWorkerService = ({bio, age, categoryList, userId, firstName, lastName, email, gender, openToWork, primarySkill}: RegisterAsWorkerService) => {
     return new Promise((resolve, reject) => {
         try {
             if(!(validateBio(bio) && validateAge(age) && validateName(firstName) && validateName(lastName) && validateEmail(email) && validateBoolean(gender) && validateBoolean(openToWork))){
                 return reject({status: 400, error: new Error("invalid inputs!")});
             }
             userId = new mongoose.Types.ObjectId(userId);
+            if(!Array.isArray(categoryList)){
+                return reject({status: 400, error: new Error("category list must be an array!")});
+            }
+            const isPrimarySkill = categoryList.filter((elem) => elem.id === primarySkill);
+            if(primarySkill && isPrimarySkill.length !== 1){
+                return reject({status: 400, error: new Error("Primary skill must be only one and should be included in the category list!")})
+            }
             User.updateOne({
                 _id: userId
             },{
@@ -89,7 +101,8 @@ export const registerAsWorkerService = ({bio, age, categoryList, userId, firstNa
                     lastName,
                     email,
                     gender,
-                    openToWork
+                    openToWork,
+                    primarySkill: new mongoose.Types.ObjectId(primarySkill)
                 }
             }).then(() => {
                 return User.findOne({_id: userId})
