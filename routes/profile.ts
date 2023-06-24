24acaf9910ee69775dc8428b6b1fad92ef7d03a8
ram33sul <rameesyounus49@gmail.com>
2023-06-24 19:49:53 +0530

    registerAsWorker now can have multiple images

--- routes/profile.ts
+++ routes/profile.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { eventHandler } from '../controllers/account';
 import { EDIT_PROFILE, GET_USER_DETAILS, OPEN_TO_WORK_OFF, OPEN_TO_WORK_ON, REGISTER_AS_WORKER } from '../controllers/events';
 import { auth } from '../authentication/auth';
-import { fileUploadMulter } from '../services/multer';
+import { fileUploadMulter, fileUploadMulterField } from '../services/multer';
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.use(auth);
 
 router.put('/edit-user-profile', fileUploadMulter('profilePicture'), eventHandler(EDIT_PROFILE));
 
-router.put('/register-as-worker', fileUploadMulter('profilePicture'), eventHandler(REGISTER_AS_WORKER));
+router.put('/register-as-worker', fileUploadMulterField(['profilePicture', 'identity']), eventHandler(REGISTER_AS_WORKER));
 
 router.patch('/open-to-work-on', eventHandler(OPEN_TO_WORK_ON));
 
--- services/multer.ts
+++ services/multer.ts
@@ -20,3 +20,26 @@ export const fileUploadMulter = (imageName: string) => {
         }
     }).single(imageName)
 }
+
+export const fileUploadMulterField = (imageNames: any) => {
+    const fields = imageNames.map((name: string) => ({name, maxCount: 1}))
+    return multer({
+        storage: multer.diskStorage({
+            destination: (req, file, cb) => {
+                const fieldDestination = fields.find((config: any) => config.name === file.fieldname)?.destination;
+                cb(null, fieldDestination || '');
+            },
+            filename: (req: any, file, cb) => {
+                const fileName = `${req.verifiedUserId}.png`;
+                cb(null, fileName)
+            }
+        }),
+        fileFilter: function(req: any, file, cb) {
+            // if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+            //   req.fileValidationError = 'Only PNG and JPEG files are allowed';
+            //   return cb(null, false);
+            // }
+            cb(null, true);
+        }
+    }).fields(fields);
+}
