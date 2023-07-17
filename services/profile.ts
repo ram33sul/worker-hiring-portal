2a89c3a17d55833e3ceae77247f1b7dce4642d53
ram33sul <rameesyounus49@gmail.com>
2023-07-17 18:11:25 +0530

    twilio keys changed

--- services/profile.ts
+++ services/profile.ts
@@ -292,9 +292,7 @@ export const getWorkersListService = ({page, pageSize, sort, rating4Plus, previo
                     }
                 }
             ]));
-            console.log(location);
             location = location?.[0] ? location[0].location : [ 0, 0 ];
-            console.log("hii");
 
             User.aggregate([
                 {
