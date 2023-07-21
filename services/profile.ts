f701fc7383199d05e7d4446baef1dc7b4f80c65c
ram33sul <rameesyounus49@gmail.com>
2023-07-22 01:35:41 +0530

    minor changes

--- services/profile.ts
+++ services/profile.ts
@@ -303,8 +303,6 @@ export const getWorkersListService = ({page, pageSize, sort, rating4Plus, previo
                 {
                     $match: {
                         proposedDate: new Date(parseInt(date)),
-                        isBeforeNoon: isBeforeNoon === 'true',
-                        isFullDay: isFullDay === 'true'
                     }
                 }
             ]);
