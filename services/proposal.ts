842238aa1b63ae8ae4534896ccb8e07dcd0a21a4
ram33sul <rameesyounus49@gmail.com>
2023-07-24 12:32:35 +0530

    minor changes

--- services/proposal.ts
+++ services/proposal.ts
@@ -279,6 +279,8 @@ export const getReportService = ({ fromDate, toDate, workHistory, hiringHistory,
     return new Promise(async (resolve, reject) => {
         try {
             const str = 'true'
+
+            console.log(fromDate, userId)
             Proposal.aggregate([
                 {
                     $match: {
